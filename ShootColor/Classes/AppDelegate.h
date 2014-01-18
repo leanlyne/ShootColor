c71c77166b02d834892c9cabab343555b7e13ad8
和均 <hejun.lhj@taobao.com>
2014-01-18 10:43:30 +0800

    添加初始文件

---
+++ ShootColor/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  ShootColor
+//
+//  Created by lihejun on 14-1-17.
+//  Copyright taobao 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ ShootColor/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShootColor
+//
+//  Created by lihejun on 14-1-17.
+//  Copyright taobao 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
